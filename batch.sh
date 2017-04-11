d63928ce0321319ac3255e296cb7e23eba5aeb4c
Jan Blechta <blechta@karlin.mff.cuni.cz>
2017-04-11 23:28:52 +0200

    Add script for reproducing the transmision problem paper

---
+++ batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python main.py NicaiseVenel -0.33333333333333333 08 | tee NicaiseVenel_-0.333_08.log
+python main.py NicaiseVenel -0.33333333333333333 16 | tee NicaiseVenel_-0.333_16.log
+python main.py NicaiseVenel -0.33333333333333333 32 | tee NicaiseVenel_-0.333_32.log
+
+python main.py NicaiseVenel -0.01 08 | tee NicaiseVenel_-0.01_08.log
+python main.py NicaiseVenel -0.01 16 | tee NicaiseVenel_-0.01_16.log
+python main.py NicaiseVenel -0.01 32 | tee NicaiseVenel_-0.01_32.log
+
+python main.py NicaiseVenel -0.99 08 | tee NicaiseVenel_-0.99_08.log
+python main.py NicaiseVenel -0.99 16 | tee NicaiseVenel_-0.99_16.log
+python main.py NicaiseVenel -0.99 32 | tee NicaiseVenel_-0.99_32.log
