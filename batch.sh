fcac96f5a262358c062981a6dc2eab9694b383f5
Jan Blechta <blechta@karlin.mff.cuni.cz>
2017-05-14 17:01:35 +0200

    Add two more refinement levels

--- batch.sh
+++ batch.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-python main.py NicaiseVenel -0.33333333333333333 08 | tee NicaiseVenel_-0.333_08.log
-python main.py NicaiseVenel -0.33333333333333333 16 | tee NicaiseVenel_-0.333_16.log
-python main.py NicaiseVenel -0.33333333333333333 32 | tee NicaiseVenel_-0.333_32.log
+python main.py NicaiseVenel -0.33333333333333333 008 | tee NicaiseVenel_-0.333_008.log
+python main.py NicaiseVenel -0.33333333333333333 016 | tee NicaiseVenel_-0.333_016.log
+python main.py NicaiseVenel -0.33333333333333333 032 | tee NicaiseVenel_-0.333_032.log
+python main.py NicaiseVenel -0.33333333333333333 064 | tee NicaiseVenel_-0.333_064.log
+python main.py NicaiseVenel -0.33333333333333333 128 | tee NicaiseVenel_-0.333_128.log
 
-python main.py NicaiseVenel -0.01 08 | tee NicaiseVenel_-0.01_08.log
-python main.py NicaiseVenel -0.01 16 | tee NicaiseVenel_-0.01_16.log
-python main.py NicaiseVenel -0.01 32 | tee NicaiseVenel_-0.01_32.log
+python main.py NicaiseVenel -0.01 008 | tee NicaiseVenel_-0.01_008.log
+python main.py NicaiseVenel -0.01 016 | tee NicaiseVenel_-0.01_016.log
+python main.py NicaiseVenel -0.01 032 | tee NicaiseVenel_-0.01_032.log
+python main.py NicaiseVenel -0.01 064 | tee NicaiseVenel_-0.01_064.log
+python main.py NicaiseVenel -0.01 128 | tee NicaiseVenel_-0.01_128.log
 
-python main.py NicaiseVenel -0.99 08 | tee NicaiseVenel_-0.99_08.log
-python main.py NicaiseVenel -0.99 16 | tee NicaiseVenel_-0.99_16.log
-python main.py NicaiseVenel -0.99 32 | tee NicaiseVenel_-0.99_32.log
+python main.py NicaiseVenel -0.99 008 | tee NicaiseVenel_-0.99_008.log
+python main.py NicaiseVenel -0.99 016 | tee NicaiseVenel_-0.99_016.log
+python main.py NicaiseVenel -0.99 032 | tee NicaiseVenel_-0.99_032.log
+python main.py NicaiseVenel -0.99 064 | tee NicaiseVenel_-0.99_064.log
+python main.py NicaiseVenel -0.99 128 | tee NicaiseVenel_-0.99_128.log
 
 
 grep ^RESULT *.log          > out.txt
