a49f9647ccff5d1d3f659eaef49107efed826bc5
Jan Blechta <blechta@karlin.mff.cuni.cz>
2017-06-23 18:46:32 +0200

    Add Bonnet-BenDhia case to batch script

--- batch.sh
+++ batch.sh
@@ -18,6 +18,11 @@ python main.py NicaiseVenel -0.99 032 | tee NicaiseVenel_-0.99_032.log
 python main.py NicaiseVenel -0.99 064 | tee NicaiseVenel_-0.99_064.log
 python main.py NicaiseVenel -0.99 128 | tee NicaiseVenel_-0.99_128.log
 
+python main.py BonnetBenDhia -5 008 | tee BonnetBenDhia_-5_008.log
+python main.py BonnetBenDhia -5 016 | tee BonnetBenDhia_-5_016.log
+python main.py BonnetBenDhia -5 032 | tee BonnetBenDhia_-5_032.log
+python main.py BonnetBenDhia -5 064 | tee BonnetBenDhia_-5_064.log
+python main.py BonnetBenDhia -5 128 | tee BonnetBenDhia_-5_128.log
 
 grep ^RESULT *.log          > out.txt
 grep -m 1 Estimators *.log >> out.txt
